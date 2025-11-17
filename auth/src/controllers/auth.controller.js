3b984336c74e22f311faddfd7ab6594f94b886a4
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-11-17 23:11:06 +0530

    register complete

--- auth/src/controllers/auth.controller.js
+++ auth/src/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const registerUser = async (req,res) => {
 
     const { username, email, password, fullName: { firstName, lastName }} = req.body;
 
+    // console.log("checking existing user")
     const isUserAlreadyExists = await userModel.findOne({
         $or: [   // It return true when either the username or email is available
             { username },
@@ -13,11 +15,14 @@ const registerUser = async (req,res) => {
     });
 
     if(isUserAlreadyExists) {
+        console.log("user exists")
         return res.status(409).json({ message: "Username or Email already exists" });
     }
 
+    // console.log('hashing password');    
     const hash = await bcrypt.hash(password, 10);
 
+    // console.log('creating user in DB')
     const user = await userModel.create({
         username, 
         email,
@@ -25,6 +30,38 @@ const registerUser = async (req,res) => {
         fullName: {
             firstName,
             lastName
+        }
+    })
+    // console.log('user created with id: ', user._id)
+
+    // if(!process.env.JWT_SECRET) {
+    //     console.warn('JWT_SECRET is not set in env');
+    // }
+
+    // console.log('signing jwt token')
+    const token = jwt.sign({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+    }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+    // console.log('sending response')
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000
+    })
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            fullName: user.fullName,
+            role: user.role,
+            addressed: user.addresses
         }
     })
 
