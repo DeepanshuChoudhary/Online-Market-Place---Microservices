47a83b1836799dc6d979c5dcfc0bbe8e3f455308
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-11-17 19:01:05 +0530

    JWT setup also bcryptjs crash

--- auth/src/controllers/auth.controller.js
+++ auth/src/controllers/auth.controller.js
@@ -1,9 +1,35 @@
 const userModel = require('../models/user.model');
+const bcrypt = require('bcryptjs');
 
 const registerUser = async (req,res) => {
 
     const { username, email, password, fullName: { firstName, lastName }} = req.body;
 
-}
+    const isUserAlreadyExists = await userModel.findOne({
+        $or: [   // It return true when either the username or email is available
+            { username },
+            { email }
+        ]
+    });
 
-module.exports = registerUser;
+    if(isUserAlreadyExists) {
+        return res.status(409).json({ message: "Username or Email already exists" });
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+        username, 
+        email,
+        password: hash,
+        fullName: {
+            firstName,
+            lastName
+        }
+    })
+
+};
+
+module.exports = {
+    registerUser
+};
