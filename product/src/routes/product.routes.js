0b462add86956fdcdf42d20e4f8b6337b87d0bb4
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-11-23 01:03:30 +0530

    product.post.test.js test cases passed all

--- product/src/routes/product.routes.js
+++ product/src/routes/product.routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const multer = require('multer');
+const productController = require('../controllers/product.controller');
+const createAuthMiddleware = require('../middlewares/auth.middleware');
+const { createProductValidators } = require('../validators/product.validators');
 
 const router = express.Router();
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 //  POST /api/products/
-router.post("/");
+router.post("/", 
+    createAuthMiddleware([ 'admin', 'seller' ]), 
+    upload.array('images', 5), 
+    createProductValidators,
+    productController.createProduct
+);
 
-
-module.experts = router;
+module.exports = router;
