f650ed431b9883e24d5bac0b39bf3ac820d92248
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-12-02 10:13:36 +0530

    payment status: COMPLETE

--- payment/src/routes/payment.routes.js
+++ payment/src/routes/payment.routes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.post("/create/:orderId", createAuthMiddleware([ "user" ]), paymentController.createPayment)
 
+router.post("/verify", createAuthMiddleware([ "user" ]), paymentController.verifyPayment)
+
 module.exports = router;
