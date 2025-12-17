947c73d6c72695310b349db04319458c02387c7f
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-12-17 09:48:18 +0530

    publishToQueue and subscribeToQueue

--- notification/src/broker/broker.js
+++ notification/src/broker/broker.js
@@ -17,8 +17,39 @@ const connect = async() => {
 
 }
 
+const publishToQueue = async (queueName, data = {}) => {
+    
+    if(!channel || !connection) await connect();
+    
+    await channel.assertQueue(queueName, {
+        durable: true
+    })
+
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(data)));
+    console.log('Message send to queue:', queueName, data);
+    
+}
+
+const subscribeToQueue = async (queueName, callback) => {
+
+    if(!channel || !connection) await connect();
+
+    await channel.assertQueue(queueName, { durable: true })
+
+    channel.consumer(queueName, async (msg) => {
+        if(msg !== null) {
+            const data = JSON.parser(msg.content.toString());
+            await callback(data);
+            channel.ack(msg);
+        }
+    })
+    
+}
+
 module.exports = {
     connect,
     channel,
-    connection
+    connection,
+    publishToQueue,
+    subscribeToQueue
 }
